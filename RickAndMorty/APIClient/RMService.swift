d5b77a7bd1f28c5739c697df78bf5149c7539c25
AlexeyManankov <virtbrain@gmail.com>
2023-09-07 07:14:58 +0500

    added - pagination work - image manager

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -23,6 +23,7 @@ final class RMService {
     
     /// Send Rick and Morty API  Call
     /// - Parameters:
+    ///   - request: Request instance
     ///   - type: The type of object we expect to get back
     ///   - completion: Callback with data or error
     public func execute<T: Codable>(_ request: RMRequest,
@@ -33,7 +34,7 @@ final class RMService {
             completion(.failure(RMServiceError.failedtoCreateRequest))
             return
         }
-        
+        print("API Call: \(request.url?.absoluteString ?? "")")
         let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
             guard let data = data, error == nil else {
                 completion(.failure(error ?? RMServiceError.failedToGetData))
