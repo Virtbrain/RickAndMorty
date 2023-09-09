41e0107e2114efe8bbe3789715cef21ca967c755
AlexeyManankov <virtbrain@gmail.com>
2023-09-09 06:58:16 +0500

    designed characters details cells

--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterDetailViewViewModel.swift
@@ -27,21 +27,34 @@ final class RMCharacterDetailViewViewModel {
         setUpSections()
     }
     
+    /**
+     let name, species, type: String
+     let status: RMCharacterStatus
+     let gender: RMCharacterGender
+     let origin: RMOrigin
+     let location: RMSingleLocation
+     let image: String
+     let episode: [String]
+     let url: String
+     let created: String
+     */
+    
     private func setUpSections() {
         sections = [
-            .photo(viewModel: .init()),
+            .photo(viewModel: .init(imageUrl: URL(string: character.image))),
             .information(viewModels: [
-                .init(),
-                .init(),
-                .init(),
-                .init()
+                .init(value: character.status.text, title: "Status"),
+                .init(value: character.gender.rawValue, title: "Gender"),
+                .init(value: character.type, title: "Type"),
+                .init(value: character.species, title: "Species"),
+                .init(value: character.origin.name, title: "Origin"),
+                .init(value: character.location.name, title: "Location"),
+                .init(value: character.created, title: "Created"),
+                .init(value: "\(character.episode.count)", title: "Total Episodes")
             ]),
-            .episodes(viewModels: [
-                .init(),
-                .init(),
-                .init(),
-                .init()
-            ]),
+            .episodes(viewModels: character.episode.compactMap {
+                return RMCharacterEpisodeCollectionViewCellViewModel(episodeDataURL: URL(string: $0))
+            }),
         ]
     }
     
--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterPhotoCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterPhotoCollectionViewCellViewModel.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 final class RMCharacterPhotoCollectionViewCellViewModel{
-    init(){}
+    private let imageUrl: URL?
+    
+    init(imageUrl: URL?){
+        self.imageUrl = imageUrl
+    }
+    
+    public func fetchImage(compition: @escaping (Result<Data, Error>) -> Void) {
+        guard let imageUrl = imageUrl else {
+            compition(.failure(URLError(.badURL)))
+            return
+        }
+        RMImageLoader.shared.downloadImage(imageUrl, completion: compition)
+    }
 }
