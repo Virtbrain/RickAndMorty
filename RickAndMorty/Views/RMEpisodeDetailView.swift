cee9703fdd883aa3c261d44b35210bf7dfbe54a4
AlexeyManankov <virtbrain@gmail.com>
2023-09-10 16:02:35 +0500

    still

---
+++ RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Alexey Manankov on 10.09.2023.
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+}
