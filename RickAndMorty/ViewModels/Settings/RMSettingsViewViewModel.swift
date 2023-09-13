003c8e56d9709d399e6f19f2adeaa5bdb2b173db
AlexeyManankov <virtbrain@gmail.com>
2023-09-13 10:24:49 +0500

    added viewModel for settings page

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Alexey Manankov on 13.09.2023.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
